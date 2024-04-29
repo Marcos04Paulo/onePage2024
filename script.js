3256686d226cb182edb92a2772af1260c7ad7009
Marcos Paulo <marcospaulo.silvatst@gmail.com>
2024-04-29 00:39:54 -0300

    Finish section about

---
+++ script.js
@@ -0,0 +1,11 @@
+let txt = document.querySelector('.show');function clicou(){
+  // document.querySelector('.show').style.display = 'block';
+  txt.classList.add('adiction');
+
+  console.log(txt);
+};
+
+function clicou(){
+  txt.classList.remove('show');
+  console.log(txt);
+}
